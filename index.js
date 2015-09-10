d63ddbfc248db657e3af75c0bf70bffa682dcfa9
otarim <otarim.com@gmail.com>
2015-09-10 13:40:23 +0800

    init

---
+++ index.js
@@ -0,0 +1,53 @@
+var Memcached = require('memcached')
+
+var promiseAble = function(fn) {
+	return new Promise(function(resolve, reject) {
+		fn(function(err, data) {
+			if (err) {
+				reject(err)
+			} else {
+				if (arguments.length > 2) {
+					data = slice.call(arguments, 1)
+				}
+				resolve(data)
+			}
+		})
+	})
+}
+
+;
+[
+	'touch',
+	'get',
+	'gets',
+	'getMulti',
+	'set',
+	'replace',
+	'add',
+	'cas',
+	'append',
+	'prepend',
+	'incr',
+	'increment',
+	'decr',
+	'decrement',
+	'del',
+	'delete',
+	'version',
+	'flush',
+	'stats',
+	'settings',
+	'slabs',
+	'items',
+	'cachedump',
+	'end'
+].forEach(function(method) {
+	var originFn = Memcached.prototype[method]
+	Memcached.prototype[method] = function() {
+		var args = [].slice.call(arguments)
+		return promiseAble(function(cb) {
+			args = args.concat(cb)
+			originFn.apply(this, args)
+		}.bind(this))
+	}
+})
---
+++ readme.md
@@ -0,0 +1,23 @@
+#co-memcached
+
+Simple wrapper to the memcached library for co-like interface(eg: koa,co)
+
+To install run:
+
+    npm install co-memcached
+
+Use in co:
+
+    var co = require('co'),
+        Memcached = require('memcached'),
+        memcached = new Memcached('127.0.0.1:11211')
+
+    require('co-memcached')
+
+    co(function*(){
+        yield memcached.set('foo','bar',1000)
+        console.log(yield memcached.get('foo'))
+    })
+
+All methods listed in [memcached docs](https://github.com/3rd-Eden/memcached#api)
+
